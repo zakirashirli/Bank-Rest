cb4fcdef7757fd01979825a89e59a84b72920cea
zakirashirli <117630695+zakirashirli@users.noreply.github.com>
2025-10-23 16:42:04 +0400

    spring project created
    all dependencies and versions are setted.

---
+++ bank-rest/src/main/java/com/bank/rest/BankRestApplication.java
@@ -0,0 +1,13 @@
+package com.bank.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankRestApplication.class, args);
+	}
+
+}
---
+++ bank-rest/src/test/java/com/bank/rest/BankRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
